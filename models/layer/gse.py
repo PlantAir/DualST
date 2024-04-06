6a2f64cfea63f4e0e04e43ce742f00d67dd6b0a9
Junzhidian <51474360+Junzhidian@users.noreply.github.com>
2024-04-06 13:43:11 +0800

    Update gse.py

--- models/layer/gse.py
+++ models/layer/gse.py
@@ -1,3 +1,13 @@
+# -------------------------------------------------------------------------
+# Copyright (c) 2021-2022, NVIDIA Corporation & Affiliates. All rights reserved.
+#
+# This work is made available under the Nvidia Source Code License.
+# To view a copy of this license, visit
+# https://github.com/NVlabs/GroupViT/blob/main/LICENSE
+#
+# Written by Jiarui Xu
+# -------------------------------------------------------------------------
+
 import math
 import torch
 import torch.nn as nn
